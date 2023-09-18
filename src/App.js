877e74940ab44a527a882e02772c6d2707f49029
Trung <trungnguyen.02082000@gmail.com>
2023-09-18 07:11:02 +0700

    tải file bài tập lên lại

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './startbs/Header';
+import Home from './startbs/Home';
+
+function App() {
+  return (
+
+    // <Header />
+    <Home />
+
+
+  );
+}
+
+export default App;
---
+++ src/startbs/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <footer>
+                    <p style={{ textAlign: "center", backgroundColor: "black", color: "white", padding: "40px", margin: 0 }}> Copyright © Your Website 2023</p>
+                </footer>
+
+            </div>
+        )
+    }
+}
